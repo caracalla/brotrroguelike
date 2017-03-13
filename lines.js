99afa87d5e01bc9ec9c50a018a72afcccf27c533
James Anderson <caracallarex@gmail.com>
2017-03-13 20:56:25 +0000

    add pixel method

--- lines.js
+++ lines.js
@@ -2,26 +2,29 @@
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext("2d");
 
+  var fillPixel = function (x, y, color) {
+    context.fillStyle = color || '#000000';
+    context.fillRect(x, y, 1, 1);
+  }
+
   var bresenham = function (startX, startY, endX, endY, color) {
     var deltaX = endX - startX;
     var deltaY = endY - startY;
     var slope = deltaY/deltaX;
-
-    context.fillStyle = color || '#000000';
 
     if (startX <= endX) {
       if (startY <= endY) {
         if (slope <= 1) {
           var deltaErr = Math.abs(slope), error = deltaErr - 0.5
           for(var x = startX, y = startY; x <= endX; x += 1) {
-            context.fillRect(x, y, 1, 1);
+            fillPixel(x, y, color);
             error += deltaErr;
             if (error >= 0.5) { y += 1; error -= 1; }
           }
         } else if (slope > 1) {
           var deltaErr = 1 / Math.abs(slope), error = deltaErr - 0.5
           for (var y = startY, x = startX; y <= endY; y += 1) {
-            context.fillRect(x, y, 1, 1);
+            fillPixel(x, y, color);
             error += deltaErr;
             if (error >= 0.5) { x += 1; error -= 1; }
           }
@@ -30,14 +33,14 @@
         if (slope >= -1) {
           var deltaErr = Math.abs(slope), error = deltaErr - 0.5
           for(var x = startX, y = startY; x <= endX; x += 1) {
-            context.fillRect(x, y, 1, 1);
+            fillPixel(x, y, color);
             error += deltaErr;
             if (error >= 0.5) { y -= 1; error -= 1; }
           }
         } else if (slope < -1) {
           var deltaErr = 1 / Math.abs(slope), error = deltaErr - 0.5
           for (var y = startY, x = startX; y >= endY; y -= 1) {
-            context.fillRect(x, y, 1, 1);
+            fillPixel(x, y, color);
             error += deltaErr;
             if (error >= 0.5) { x += 1; error -= 1; }
           }
@@ -48,14 +51,14 @@
         if (slope >= -1) {
           var deltaErr = Math.abs(slope), error = deltaErr - 0.5
           for(var x = startX, y = startY; x >= endX; x -= 1) {
-            context.fillRect(x, y, 1, 1);
+            fillPixel(x, y, color);
             error += deltaErr;
             if (error >= 0.5) { y += 1; error -= 1; }
           }
         } else if (slope < -1) {
           var deltaErr = 1 / Math.abs(slope), error = deltaErr - 0.5
           for (var y = startY, x = startX; y <= endY; y += 1) {
-            context.fillRect(x, y, 1, 1);
+            fillPixel(x, y, color);
             error += deltaErr;
             if (error >= 0.5) { x -= 1; error -= 1; }
           }
@@ -64,14 +67,14 @@
         if (slope <= 1) {
           var deltaErr = Math.abs(slope), error = deltaErr - 0.5
           for(var x = startX, y = startY; x >= endX; x -= 1) {
-            context.fillRect(x, y, 1, 1);
+            fillPixel(x, y, color);
             error += deltaErr;
             if (error >= 0.5) { y -= 1; error -= 1; }
           }
         } else if (slope > 1) {
           var deltaErr = 1 / Math.abs(slope), error = deltaErr - 0.5
           for (var y = startY, x = startX; y >= endY; y -= 1) {
-            context.fillRect(x, y, 1, 1);
+            fillPixel(x, y, color);
             error += deltaErr;
             if (error >= 0.5) { x -= 1; error -= 1; }
           }
@@ -80,5 +83,6 @@
     }
   };
 
+  window.pixel = fillPixel;
   window.line = bresenham;
 })();
